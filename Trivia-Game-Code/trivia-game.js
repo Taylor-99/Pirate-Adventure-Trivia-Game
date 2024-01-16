1251634eff501c2d1c92c3a192d6124728b4b321
User Taylor <elexus0201@gmail.com>
2024-01-15 22:58:12 -0500

    working welcome screen buttons

--- Trivia-Game-Code/trivia-game.js
+++ Trivia-Game-Code/trivia-game.js
@@ -1,1 +1,133 @@
-console.log("I'm working")
+class Pirates {
+    constructor(name, crew, position){
+        this.name = name;
+        this.crew = crew
+        this.position = position;
+        this.health = 100;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getCrew(){
+        return this.crew;
+    }
+
+    getHealth(){
+        return this.health;
+    }
+
+}
+
+class Captain extends Pirates{
+    constructor(name, crew, position){
+        super(name, crew, position)
+        this.doubloons = 0;
+        this.crewMembers = [];
+    }
+
+    getDoubloons(){
+        return this.doubloons;
+    }
+}
+
+class YourPirates extends Pirates {
+    constructor(name, crew, position){
+        super(name, crew, position)
+        this.weapon = weapon;
+    }
+
+    getWeapon(weapon){
+        this.weapon = weapon;
+    }
+}
+
+function introduction(){
+
+    let introSection = document.getElementById("intro");
+
+    introSection.style.backgroundColor = "white";
+
+    let introText = document.createElement("p");
+
+    introSection.appendChild(introText);
+    introText.setAttribute("id", "intro-text");
+
+
+    introText.innerHTML = "<p>Yo-ho, matey! Welcome to the treacherous seas of the Pirate Adventure Trivia Game! <br><br> Embark on a daring voyage full of swashbuckling exploits, hidden treasures, and perilous encounters. As the captain of your own ship, you'll navigate through mysterious islands and engage in fierce naval 	battles.<br><br> Prepare to make crucial decisions that will shape your pirate legacy. Will you play and be a legendary captain known throughout the seas, or quit now and stay on land?</p>";
+
+    let introButtons = document.createElement("div");
+
+    introSection.appendChild(introButtons);
+    introButtons.setAttribute("class", "intro-button");
+
+    let playButton = document.createElement("button");
+    playButton.textContent = "Let's Go";
+    playButton.setAttribute("id", "yes-play");
+
+    let quitButton = document.createElement("button");
+    quitButton.textContent = "Stay on Land";
+    quitButton.setAttribute("id", "no-quit");
+
+    introButtons.appendChild(playButton);
+    introButtons.appendChild(quitButton);
+
+    introButtons.addEventListener("click", function(event) {
+        event.preventDefault();
+        let playOption = event.target;
+
+        if(playOption.tagName !== "BUTTON"){
+            return;
+        }
+
+        console.log(playOption);
+
+        let playChoice = playOption.getAttribute("id");
+
+        if(playChoice === "yes-play"){
+            console.log("They want to play");
+        }
+        else if(playChoice === "no-quit"){
+            console.log("They want to quit");
+        }
+    })
+}
+
+introduction()
+
+// const crewArr = [
+//     ["Red-Eyed Scarlet", "First-Mate"],
+//     ["Silverbeard", "Quartermaster"],
+//     ["Sharktooth Morgan", "Boatswain"],
+//     ["Pegleg Charlie", "Navigator"],
+//     ["Wind Walker Grace", "Gunner"],
+//     ["Doctor Iron Bones", "Doctor"]
+// ];
+
+// const weaponsArr = [
+//     ["Fist", 0],
+//     ["Cutlass", 0],
+//     ["Rapier", 0],
+//     ["Dagger", 0],
+//     ["War Hammer", 0],
+//     ["Boarding Pike", 0]
+// ];
+
+// const opponentArr = [
+//     ["Captain Vanguard", "Shadow Serpents", "Captain"],
+//     ["Buccaneer Nell", "Black Tide Buccaneers", "Boatswain"],
+//     ["Blackheart Brutus", "Thunderstrike Raiders", "Navigator"],
+
+//     [["Ironjaw Grimsby", "Stormborn Marauders", "Boatswain"], ["Shadowblade Killian", "Stormborn Marauders", "First Mate"]],
+//     [["Darkwater Mortimer", "Crossbones Syndicate", "Quartermaster"], ["Deathwind Harrow", "Crossbones Syndicate", "Gunner"]],
+//     [["Direclaw Riven", "Ironheart Privateers", "First Mate"], ["Wrathbringer Argyle", "Ironheart Privateers", "Navigator"]],
+
+//     [["Vilethorn Crag", "Sable Phantom Fleet", "Quatermaster"], ["Grimshade Blackthorn", "Sable Phantom Fleet", "Gunner"], ["Sablebane Morgana", "Sable Phantom Fleet", "Captain"]]
+
+// ];
+
+// let captainName = "";
+// let crewName = "";
+
+// let captain = new Captain(captainName, crewName, "Captain");
